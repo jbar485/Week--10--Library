f843be2a23111f761d6a9d7b9ddad40f2f3d410a
Student <example@example.com>
2020-01-08 09:55:11 -0800

    add a backup database

---
+++ database_backup.sql
@@ -0,0 +1,351 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.1
+-- Dumped by pg_dump version 12.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: authors; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.authors (
+    name character varying,
+    id integer NOT NULL
+);
+
+
+ALTER TABLE public.authors OWNER TO "Guest";
+
+--
+-- Name: authors_books; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.authors_books (
+    author_id integer,
+    book_id integer,
+    id integer NOT NULL
+);
+
+
+ALTER TABLE public.authors_books OWNER TO "Guest";
+
+--
+-- Name: authors_books_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.authors_books_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.authors_books_id_seq OWNER TO "Guest";
+
+--
+-- Name: authors_books_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.authors_books_id_seq OWNED BY public.authors_books.id;
+
+
+--
+-- Name: authors_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.authors_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.authors_id_seq OWNER TO "Guest";
+
+--
+-- Name: authors_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.authors_id_seq OWNED BY public.authors.id;
+
+
+--
+-- Name: books; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.books (
+    name character varying,
+    id integer NOT NULL,
+    patron_id integer,
+    return_date timestamp without time zone,
+    checkout_date timestamp without time zone
+);
+
+
+ALTER TABLE public.books OWNER TO "Guest";
+
+--
+-- Name: books_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.books_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.books_id_seq OWNER TO "Guest";
+
+--
+-- Name: books_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.books_id_seq OWNED BY public.books.id;
+
+
+--
+-- Name: books_patrons; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.books_patrons (
+    patron_id integer,
+    book_id integer,
+    id integer NOT NULL
+);
+
+
+ALTER TABLE public.books_patrons OWNER TO "Guest";
+
+--
+-- Name: books_patrons_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.books_patrons_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.books_patrons_id_seq OWNER TO "Guest";
+
+--
+-- Name: books_patrons_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.books_patrons_id_seq OWNED BY public.books_patrons.id;
+
+
+--
+-- Name: patrons; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.patrons (
+    name character varying,
+    id integer NOT NULL
+);
+
+
+ALTER TABLE public.patrons OWNER TO "Guest";
+
+--
+-- Name: patrons_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.patrons_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.patrons_id_seq OWNER TO "Guest";
+
+--
+-- Name: patrons_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.patrons_id_seq OWNED BY public.patrons.id;
+
+
+--
+-- Name: authors id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.authors ALTER COLUMN id SET DEFAULT nextval('public.authors_id_seq'::regclass);
+
+
+--
+-- Name: authors_books id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.authors_books ALTER COLUMN id SET DEFAULT nextval('public.authors_books_id_seq'::regclass);
+
+
+--
+-- Name: books id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.books ALTER COLUMN id SET DEFAULT nextval('public.books_id_seq'::regclass);
+
+
+--
+-- Name: books_patrons id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.books_patrons ALTER COLUMN id SET DEFAULT nextval('public.books_patrons_id_seq'::regclass);
+
+
+--
+-- Name: patrons id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.patrons ALTER COLUMN id SET DEFAULT nextval('public.patrons_id_seq'::regclass);
+
+
+--
+-- Data for Name: authors; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.authors (name, id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: authors_books; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.authors_books (author_id, book_id, id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: books; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.books (name, id, patron_id, return_date, checkout_date) FROM stdin;
+\.
+
+
+--
+-- Data for Name: books_patrons; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.books_patrons (patron_id, book_id, id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: patrons; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.patrons (name, id) FROM stdin;
+\.
+
+
+--
+-- Name: authors_books_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.authors_books_id_seq', 1, false);
+
+
+--
+-- Name: authors_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.authors_id_seq', 1, false);
+
+
+--
+-- Name: books_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.books_id_seq', 1, false);
+
+
+--
+-- Name: books_patrons_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.books_patrons_id_seq', 1, false);
+
+
+--
+-- Name: patrons_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.patrons_id_seq', 1, false);
+
+
+--
+-- Name: authors_books authors_books_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.authors_books
+    ADD CONSTRAINT authors_books_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: authors authors_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.authors
+    ADD CONSTRAINT authors_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: books_patrons books_patrons_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.books_patrons
+    ADD CONSTRAINT books_patrons_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: books books_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.books
+    ADD CONSTRAINT books_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: patrons patrons_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.patrons
+    ADD CONSTRAINT patrons_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
